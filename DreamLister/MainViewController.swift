c8e048b989c50117bcfb6907f46b980e9b2f6377
Luis Santos <lfsantos2008@Luiss-MacBook-Pro.local>
2019-05-27 15:21:07 -0500

    adding new files

--- DreamLister/MainViewController.swift
+++ DreamLister/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  DreamLister
 //
 //  Created by Luis Santos on 5/24/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
