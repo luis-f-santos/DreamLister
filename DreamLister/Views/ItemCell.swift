461e9540e3665816481dcc66a5142df62b819a18
Luis Santos <lfsantos2008@Luiss-MacBook-Pro.local>
2019-05-27 15:38:24 -0500

    Adding hookups to UI

---
+++ DreamLister/Views/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Luis Santos on 5/27/19.
+//  Copyright © 2019 Luis Santos. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var detail: UILabel!
+}
